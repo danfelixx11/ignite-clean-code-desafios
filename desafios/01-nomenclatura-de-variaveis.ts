74bf3693a98b22521c4a7fd9ab528df3da15ff86
Daniel <dan_felixx@hotmail.com>
2025-10-28 17:35:59 -0300

    refactor(desafio-01): apply clean code principles to variable naming

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,6 +1,6 @@
 // Nomenclatura de variáveis
 
-const list = [
+const githubPopularityCategories = [
   {
     title: 'User',
     followers: 5
@@ -19,37 +19,37 @@ const list = [
   },
 ]
 
-export default async function getData(req, res) {
-  const github = String(req.query.username)
+export default async function findGithubUserCategory(req, res) {
+  const username = String(req.query.username)
 
-  if (!github) {
+  if (!username) {
     return res.status(400).json({
       message: `Please provide an username to search on the github API`
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const response = await fetch(`https://api.github.com/users/${username}`);
 
   if (response.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
+      message: `User with username "${username}" not found`
     })
   }
 
-  const data = await response.json()
+  const githubUserData = await response.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const sortedCategories = githubPopularityCategories.sort((categoryA, categoryB) =>  categoryB.followers - categoryA.followers); 
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const matchingCategory = sortedCategories.find(category => githubUserData.followers > category.followers)
 
-  const result = {
-    github,
-    category: category.title
+  const userProfile = {
+    github: username,
+    category: matchingCategory.title
   }
 
-  return result
+  return userProfile;
 }
 
-getData({ query: {
+findGithubUserCategory({ query: {
   username: 'josepholiveira'
 }}, {})
